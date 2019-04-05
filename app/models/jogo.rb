31bcb12e0be43da5379f43c3a7f7b0f706ade8b2
matheus <matheusholscher@gmail.com>
2019-04-04 22:03:24 -0300

    segundo commit

--- app/models/jogo.rb
+++ app/models/jogo.rb
@@ -2,12 +2,13 @@ class Jogo < ApplicationRecord
 
     belongs_to :classificacao
     has_many :bibliotecas_de_usuario, class_name: "Biblioteca"
+    
 
     validates :titulo, :desenvolvedora, :descricao, :lancamento, presence: true
    
 
     scope :por_titulo, lambda { |titulo = nil|
-    
+
             where(titulo: titulo) if titulo
     }
 
